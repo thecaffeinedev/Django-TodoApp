e99688c3e79cfa110ff93d92234d90a797ae180b
Prabhat <prabhat1015@gmail.com>
2020-05-07 20:18:58 +0530

    Pushing Part-2 Tutorial Code

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World !")
+def signupuser(request):
+
+    return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
